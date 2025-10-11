94e1a384449fd6aaaa0647a3c246c4902216236f
Ye Yu <nearjilt@gmail.com>
2025-10-11 15:18:35 +0800

    style: update homepage header styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,59 @@
 import type { ReactNode } from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
+import Head from "@docusaurus/Head";
+import Link from "@docusaurus/Link";
+import { SpoonOSLogo } from "../components/shared/spoon-os-logo";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("")}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={""}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/quick-start"
-          >
-            Get Started 🚀
-          </Link>
-          <Link
-            className="button button--primary button--lg"
-            to="/docs/getting-started/installation"
-          >
-            Quick Install ⚡
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
+// TODO: font family
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
+
   return (
-    <Layout
-      title={`${siteConfig.title} - Code Cook Book`}
-      description="SpoonOS Core Developer Framework - Agentic OS for the Sentient Economy. Build powerful AI agents with Web3 integration."
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <>
+      <Head>
+        <title>{`${siteConfig.title} - Code Cook Book`}</title>
+        <meta
+          name="description"
+          content="SpoonOS Core Developer Framework - Agentic OS for the Sentient Economy. Build powerful AI agents with Web3 integration."
+        />
+      </Head>
+
+      {/* landing page container */}
+      <div className="bg-black min-h-screen max-w-screen">
+        {/* header */}
+        {/* TODO: hover style */}
+        <header className="h-[110px] flex items-center">
+          <div
+            className="flex justify-between items-center max-w-[1568px] w-full mx-20"
+            style={{
+              borderBottom: "1px solid white",
+            }}
+          >
+            <h1 className="flex gap-[18px] items-end">
+              <SpoonOSLogo className="size-12" />
+              <a href="/" className="text-white">
+                SpoonOS
+              </a>
+            </h1>
+
+            <div className="flex gap-[46px] items-center text-2xl font-medium">
+              <Link
+                href="docs/getting-started/installation/"
+                className="text-white"
+              >
+                Document
+              </Link>
+              <a
+                href="https://github.com/XSpoonAi/spoon-core"
+                target="_blank"
+                className="text-white"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
