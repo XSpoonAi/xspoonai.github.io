f427d812af2305af7c93ede7a2e5452464fa0de3
Ye Yu <nearjilt@gmail.com>
2025-10-11 17:36:03 +0800

    feat: add background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,10 @@ import { GradientCard } from "../components/shared/gradient-card";
 import { Card1Brain } from "../svgs/card-1-brain";
 import { Card2Atom } from "../svgs/card-2-atom";
 import { Card3Cube } from "../svgs/card-3-cube";
+import { HomeBg } from "../components/shared/home-bg";
 
-// TODO: font family
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
-  // static/img/landing-bg.jpg
   const cards: Array<{
     title: string;
     icon: FC<SVGProps<SVGSVGElement>>;
@@ -104,11 +103,8 @@ export default function Home(): ReactNode {
         />
       </Head>
 
-      {/* landing page container */}
       <div className="bg-black min-h-screen max-w-screen">
-        {/* header */}
-        {/* TODO: hover style */}
-        <header className="h-[110px] flex items-center mb-10 justify-center">
+        <header className="h-[110px] flex items-center mb-10 justify-center relative z-10">
           <div
             className="flex justify-between items-center max-w-[1568px] w-full mx-20"
             style={{
@@ -140,37 +136,41 @@ export default function Home(): ReactNode {
           </div>
         </header>
 
-        {/* main */}
-        <main className="flex flex-col">
-          <h2 className="text-white mx-auto text-4xl">SpoonOS</h2>
+        <main
+          className="flex flex-col py-[200px] min-h-[1333px]"
+          style={{
+            backgroundImage: 'url("/img/home-bg.jpg")',
+            backgroundSize: "100% auto",
+            backgroundPosition: "top",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <h2 className="text-white mx-auto text-4xl relative z-10">SpoonOS</h2>
 
-          <h2 className="flex flex-col text-[100px] font-bold bg-[linear-gradient(92.67deg,#58FF98_0%,#59FF98_8.04%,#5AFF9A_15.48%,#5DFF9C_22.42%,#61FEA0_28.94%,#66FEA4_35.13%,#6CFDAA_41.1%,#73FDB0_46.93%,#7AFCB6_52.71%,#83FBBE_58.54%,#8CFAC6_64.51%,#96F9CF_70.71%,#A1F8D9_77.23%,#ACF7E3_84.16%,#B8F6ED_91.6%,#C4F5F8_99.64%)] bg-clip-text text-transparent mx-auto mt-7 mb-8">
+          <h2 className="flex flex-col text-[100px] font-bold bg-[linear-gradient(92.67deg,#58FF98_0%,#59FF98_8.04%,#5AFF9A_15.48%,#5DFF9C_22.42%,#61FEA0_28.94%,#66FEA4_35.13%,#6CFDAA_41.1%,#73FDB0_46.93%,#7AFCB6_52.71%,#83FBBE_58.54%,#8CFAC6_64.51%,#96F9CF_70.71%,#A1F8D9_77.23%,#ACF7E3_84.16%,#B8F6ED_91.6%,#C4F5F8_99.64%)] bg-clip-text text-transparent mx-auto mt-7 mb-8 relative z-10">
             <span className="text-center">Agentic OS for a</span>
             <span className="text-center">Sentient Economy</span>
           </h2>
 
-          {/* TODO: hover style */}
-          <div className="flex gap-5 mx-auto">
-            {/* Get Started */}
+          <div className="flex gap-5 mx-auto relative z-10">
             <Link
               href="docs/getting-started/quick-start/"
               className="relative inline-block rounded-[50px] px-[30px] py-3 text-2xl font-medium text-[#B1FFCF] bg-transparent hover:no-underline"
               style={{
                 position: "relative",
-                zIndex: 0,
+                zIndex: 10,
               }}
             >
               Get Started
               <span className={styles.gradientBorder} />
             </Link>
 
-            {/* Quick Install */}
             <Link
               href="docs/getting-started/installation/"
               className="relative inline-block rounded-[50px] px-[30px] py-3 text-2xl font-medium text-[#B1FFCF] bg-transparent  hover:no-underline"
               style={{
                 position: "relative",
-                zIndex: 0,
+                zIndex: 10,
               }}
             >
               Quick Install
@@ -179,7 +179,7 @@ export default function Home(): ReactNode {
           </div>
         </main>
 
-        <section className="flex max-w-[1172px] gap-5 mx-auto justify-between mt-[150px] mb-[250px]">
+        <section className="flex max-w-[1172px] gap-5 mx-auto justify-between mt-[150px] mb-[250px] relative z-10">
           {cards.map((v, i) => (
             <GradientCard
               key={i}
@@ -190,7 +190,7 @@ export default function Home(): ReactNode {
           ))}
         </section>
 
-        <footer className="flex flex-col gap-[60px] mx-auto text-white max-w-[600px]">
+        <footer className="flex flex-col gap-[60px] mx-auto text-white max-w-[600px] relative z-10">
           <div className="flex gap-5 mx-auto text-white justify-center w-full">
             {footerLinks.map((v) => (
               <div className="flex flex-col flex-1 pl-[30px]">
