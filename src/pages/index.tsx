8b9cf6f93c9ab1e8252e58f26cc77efc0b6229f1
Ye Yu <nearjilt@gmail.com>
2025-10-11 15:45:31 +0800

    feat: set base styles for homepage main text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Head from "@docusaurus/Head";
 import Link from "@docusaurus/Link";
 import { SpoonOSLogo } from "../components/shared/spoon-os-logo";
+import styles from "./index.module.css";
 
 // TODO: font family
 export default function Home(): ReactNode {
@@ -22,7 +23,7 @@ export default function Home(): ReactNode {
       <div className="bg-black min-h-screen max-w-screen">
         {/* header */}
         {/* TODO: hover style */}
-        <header className="h-[110px] flex items-center">
+        <header className="h-[110px] flex items-center mb-10">
           <div
             className="flex justify-between items-center max-w-[1568px] w-full mx-20"
             style={{
@@ -53,6 +54,45 @@ export default function Home(): ReactNode {
             </div>
           </div>
         </header>
+
+        {/* main */}
+        <main className="flex flex-col">
+          <h2 className="text-white mx-auto text-4xl">SpoonOS</h2>
+
+          <h2 className="flex flex-col text-[100px] font-bold bg-[linear-gradient(92.67deg,#58FF98_0%,#59FF98_8.04%,#5AFF9A_15.48%,#5DFF9C_22.42%,#61FEA0_28.94%,#66FEA4_35.13%,#6CFDAA_41.1%,#73FDB0_46.93%,#7AFCB6_52.71%,#83FBBE_58.54%,#8CFAC6_64.51%,#96F9CF_70.71%,#A1F8D9_77.23%,#ACF7E3_84.16%,#B8F6ED_91.6%,#C4F5F8_99.64%)] bg-clip-text text-transparent mx-auto mt-7 mb-8">
+            <span className="text-center">Agentic OS for a</span>
+            <span className="text-center">Sentient Economy</span>
+          </h2>
+
+          {/* TODO: hover style */}
+          <div className="flex gap-5 mx-auto">
+            {/* Get Started */}
+            <Link
+              href="docs/getting-started/quick-start/"
+              className="relative inline-block rounded-[50px] px-[30px] py-3 text-2xl font-medium text-[#B1FFCF] bg-transparent hover:no-underline"
+              style={{
+                position: "relative",
+                zIndex: 0,
+              }}
+            >
+              Get Started
+              <span className={styles.gradientBorder} />
+            </Link>
+
+            {/* Quick Install */}
+            <Link
+              href="docs/getting-started/installation/"
+              className="relative inline-block rounded-[50px] px-[30px] py-3 text-2xl font-medium text-[#B1FFCF] bg-transparent  hover:no-underline"
+              style={{
+                position: "relative",
+                zIndex: 0,
+              }}
+            >
+              Quick Install
+              <span className={styles.gradientBorder} />
+            </Link>
+          </div>
+        </main>
       </div>
     </>
   );
